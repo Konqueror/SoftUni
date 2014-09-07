71fe53eeabd91418557609844835e2d957d5f0f3
Roumen Damianoff <roumen@dawebs.com>
2014-09-07 19:40:24 +0300

    added c# exam 2014.07.25

---
+++ C# Basics/Exams/2014.07.25_Evening/Electricity/Electricity/Electricity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Electricity
+{
+    class Electricity
+    {
+        static void Main(string[] args)
+        {
+            int floors = int.Parse(Console.ReadLine());
+            int flats = int.Parse(Console.ReadLine());
+            double watts = 0;
+
+            string timeString = Console.ReadLine();
+
+            timeString = timeString.Replace(":","");
+
+            
+            int time = Convert.ToInt32(timeString);
+
+            if (time >= 1400 && time <= 1859)
+            {
+                // case 1
+                watts = (2 * 100.53 + 2 * 125.9)*floors*flats;
+            }
+            else if (time >= 1900 && time <= 2359)
+            {
+                // case 2
+                watts = (7 * 100.53 + 6 * 125.9) * floors * flats;
+            }
+            else if (time >= 0 && time <= 859)
+            {
+                // case 3
+                watts = (1 * 100.53 + 8 * 125.9) * floors * flats;
+            }
+
+            Console.WriteLine((int)watts + " Watts");
+            
+        }
+    }
+}
---
+++ C# Basics/Exams/2014.07.25_Evening/OddAndEvenJumps/OddAndEvenJumps/OddAndEvenJumps.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddAndEvenJumps
+{
+    class OddAndEvenJumps
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+            inputString = inputString.ToLower();
+            inputString = inputString.Replace(" ","");
+            int oddJump = int.Parse(Console.ReadLine());
+            int evenJump = int.Parse(Console.ReadLine());
+
+            int oddJumpStep = oddJump;
+            int evenJumpStep = evenJump;
+
+            string oddString = "";
+            string evenString = "";
+
+            ulong odd = 0;
+            ulong even = 0;
+
+            bool isOdd = true;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (isOdd)
+                {
+                    oddString += inputString[i];
+                    isOdd = false;
+                }
+                else
+                {
+                    evenString += inputString[i];
+                    isOdd = true;
+                }
+            }
+
+            for (int i = 0; i < oddString.Length; i++)
+            {
+                if (i+1 != oddJump)
+                {
+                    odd += (ulong)oddString[i];
+                }
+                else
+                {
+                    odd *= (ulong)oddString[i];
+                    oddJump += oddJumpStep;
+                }
+                
+            }
+
+            for (int i = 0; i < evenString.Length; i++)
+            {
+                if (i + 1 != evenJump)
+                {
+                    even += (ulong)evenString[i];
+                }
+                else
+                {
+                    even *= (ulong)evenString[i];
+                    evenJump += evenJumpStep;
+                }
+
+            }
+
+            Console.WriteLine("Odd: " + odd.ToString("X"));
+            Console.WriteLine("Even: " + even.ToString("X"));
+        }
+    }
+}
