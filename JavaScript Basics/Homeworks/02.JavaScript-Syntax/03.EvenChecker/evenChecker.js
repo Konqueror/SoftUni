b937787ef2646295a6c540ca6f47e0844c884a1e
Roumen Damianoff <roumen@dawebs.com>
2014-11-07 14:01:28 +0300

    added EvenChecker

---
+++ JavaScript Basics/Homeworks/02.JavaScript-Syntax/03.EvenChecker/evenChecker.js
@@ -0,0 +1,15 @@
+evenNumber(3);
+//evenNumber(127);
+//evenNumber(588);
+
+function evenNumber(number)
+{
+	if (number%2==0)
+	{
+		console.log("true")
+	}
+	else
+	{
+		console.log("false");
+	}
+}
