04114a13683504b7ffb048933874c86770c8cff8
Roumen Damianoff <roumen@dawebs.com>
2014-12-01 11:03:22 +0200

    added Install-PHP homework

---
+++ PHP Basics/Homeworks/01.Install-PHP/Numbers.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+	<body>
+
+		<div>
+			<?php for ($i=1; $i<101; $i++) : ?>
+			<?php echo $i; ?><br>
+			<?php endfor; ?>
+		</div>
+
+	</body>
+</html>
